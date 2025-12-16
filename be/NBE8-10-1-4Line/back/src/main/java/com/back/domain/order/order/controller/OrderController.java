84b351291f9e4b826b55213d502285486921993d
Minhee Han <yyj9694651@gmail.com>
2025-12-16 11:47:57 +0900

    Feat: Implement OrderController, OrderService, and OrderRepository with basic structure

---
+++ be/NBE8-10-1-4Line/back/src/main/java/com/back/domain/order/order/controller/OrderController.java
@@ -0,0 +1,15 @@
+package com.back.domain.order.order.controller;
+
+import com.back.domain.order.order.service.OrderService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
+@RequiredArgsConstructor
+@Tag(name = "OrderController", description = "API 주문 컨트롤러")
+public class OrderController {
+    private final OrderService orderService;
+}
---
+++ be/NBE8-10-1-4Line/back/src/main/java/com/back/domain/order/order/service/OrderService.java
@@ -0,0 +1,11 @@
+package com.back.domain.order.order.service;
+
+import com.back.domain.order.order.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+    private final OrderRepository orderRepository;
+}
