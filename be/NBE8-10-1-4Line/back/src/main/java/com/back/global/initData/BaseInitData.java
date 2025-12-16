528d8424002630acd1e39b9382ef86e36927bcc5
Minhee Han <yyj9694651@gmail.com>
2025-12-16 17:36:26 +0900

    Feat: Add .gitignore, BaseInitData for initial product setup, ResponseAspect for handling responses

---
+++ be/NBE8-10-1-4Line/back/src/main/java/com/back/global/initData/BaseInitData.java
@@ -0,0 +1,34 @@
+package com.back.global.initData;
+
+import com.back.domain.product.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.transaction.annotation.Transactional;
+
+@Configuration
+@RequiredArgsConstructor
+public class BaseInitData {
+    @Autowired
+    @Lazy
+    private BaseInitData self;
+    private final ProductService productService;
+
+    @Bean
+    ApplicationRunner baseInitDataApplicationRunner() {
+        return args -> {
+            self.work1();
+        };
+    }
+
+    @Transactional
+    public void work1() {
+        if (productService.count() > 0) return;
+
+        productService.create("columbia", 3000, "columbia coffee beans.");
+        productService.create("콜롬비아", 3000, "콜롬비아산 원두입니다.");
+    }
+}
