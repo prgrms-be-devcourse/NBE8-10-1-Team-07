21ca1d6841886580c57941600f85d502e38a26d5
Minhee Han <yyj9694651@gmail.com>
2025-12-16 17:37:15 +0900

    Feat: Add Product entity with no-args constructor and creation method; implement ProductService for product management

--- be/NBE8-10-1-4Line/back/src/main/java/com/back/domain/product/product/entity/Product.java
+++ be/NBE8-10-1-4Line/back/src/main/java/com/back/domain/product/product/entity/Product.java
@@ -5,11 +5,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
 @Table(name = "product")
 public class Product extends BaseEntity {
 
@@ -21,4 +23,19 @@ public class Product extends BaseEntity {
 
     @Column(columnDefinition = "TEXT")
     private String description;
+
+    public Product(String name, int price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public static Product create(String name, int price, String description) {
+        Product product = new Product();
+        product.name = name;
+        product.price = price;
+        product.description = description;
+
+        return product;
+    }
 }
